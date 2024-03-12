d38a8d7d70f323017e374906eeb20e343795b037
AbdelBt <abdella.boutaarourt@hotmail.com>
2024-03-12 18:22:42 +0100

    fix

--- components/Newest.tsx
+++ components/Newest.tsx
@@ -28,7 +28,6 @@ export default async function Newest() {
           </h2>
           <Link className="flex text-primary items-center gap-x-1" href="/tous">
           Voir Tous{" "}
-            Voir Tous{" "}
             <span>
               <ArrowRight />
             </span>
